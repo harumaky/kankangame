69548708dfc4fc08b091a3dfde24efbcc59d23c6
harumaky <harumakiprog@gmail.com>
2020-04-30 17:51:56 +0900

    ボックスとの上下左右判定、カウント0で削除機能

--- game.js
+++ game.js
@@ -18,7 +18,11 @@ $(function() {
   canvas.style.width = String(canvas.width / dpr) + "px";
   canvas.style.height = String(canvas.height / dpr) + "px";
 
-  let theBall, timerId;
+  let theBall, 
+      timerId,
+      box_x, box_y, box_id;
+  let boxes = [];
+  let reflectedJustBefore = false;
 
 
   class Ball {
@@ -35,22 +39,111 @@ $(function() {
       ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, true);
       ctx.fill();
     }
-    move() {
+    wallReflect() {
       this.x += this.vx;
       this.y += this.vy;
       // 右、左衝突
       if (this.x + this.r > canvas.width / dpr || this.x - this.r < 0) {
-        this.vx *= -1;
+        this.vx *= -1
       }
       // 上
-      if (this.y - this.r < 0) { this.vy *= -1; }
+      if (this.y - this.r < 0) { 
+        this.vy *= -1
+      }
+      // 下
+      if (this.y + this.r > canvas.height / dpr) { this.vy *= -1; }
     }
   }
-  
+  class box {
+    constructor(x, y, num, id) {
+      this.x = x;
+      this.y = y;
+      this.num = num;
+      this.id = id;
+      this.color = `rgb(${rand(100, 255)}, 50, 40)`;
+    }
+    draw() {
+      // 30 * 30
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, 30, 30);
+      ctx.fillStyle = 'white';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(this.num, this.x + 15, this.y + 15);
+    }
+    checkBall() {
+      let theX = theBall.x;
+      let theY = theBall.y;
+      let theR = theBall.r;
+
+      // 角判定を導入する
+      if (!reflectedJustBefore) {
+        // ボールの右端がボックスの左端より大きいかつボールの左端がボックスの右端より小さいとき、ボールがボックスの高さの中にある->左右に当たった
+        // (ボックスの幅内に入る&&それが高さ内にある)
+        if (theX + theR > this.x && theX - theR < this.x + 30) {
+          console.log('左右1: ボックスのx域に入った');
+          if (theY - theR > this.y && theY + theR < this.y + 30) {
+            this.metBox();
+            theBall.vx *= -1;
+            console.log('左右2: 左右に当たった！');
+          }
+        }
+      }
+      if (!reflectedJustBefore) {
+        // ボールの下端がボックスの上端より大きいかつボールの上端がボックスの下端より小さいとき、ボールがボックスの幅の中にある->上下に当たった
+        // (ボックスの高さ内に入る&&それが幅内にある)
+        if (theY + theR > this.y && theY - theR < this.y + 30) {
+          console.log('上下1: ボックスのy域に侵入');
+          // ボールの右端がボックスの左端より大きいかつボールの左端がボックスの右端よりも小さい
+          if (theX + theR > this.x + 3 && theX - theR < this.x + 27) {
+            // 左右に当たった後、続けて上下も当たり判定起こらないように、ボックスの幅を小さく仮定する
+            this.metBox();
+            theBall.vy *= -1;
+            console.log('上下2: 上下に当たった！');
+          }
+        }
+      }
+    }
+    metBox() {
+      reflectedJustBefore = true;
+      this.num--;
+      if (this.num === 0) {
+        console.log('0!!!');
+        this.deleteBox();
+      }
+      setTimeout(() => { 
+        reflectedJustBefore = false
+        console.log('not just before');
+      }, 100);
+    }
+
+    deleteBox() {
+      let thisId = this.id;
+      boxes.filter(function(item, index) {
+        if (item.id === thisId) {
+          boxes.splice(index, 1);
+        }
+      });
+    }
+  }
+
 
   function gameInit() {
-    theBall = new Ball(rand(50, 250), 400, 2, -2);
+    theBall = new Ball(rand(100, 200), 400, 5, -5);
+    setBoxes();
+    console.log(boxes);
     update();
+  }
+  function setBoxes() {
+    for(let i = 0; i < 4; i++) {
+      box_x = 80 * i + 100;
+      for(let j = 0; j < 4; j++) {
+        box_y = 80 * j + 100;
+        box_id = boxes.length;
+        // idは要素が消えた後lengthと整合性なくなる
+        boxes.push( new box(box_x, box_y, 5, box_id) );
+      }
+    }
   }
 
   function clearField() {
@@ -59,16 +152,29 @@ $(function() {
   function update() {
     clearField();
     theBall.draw();
-    theBall.move();
+    theBall.wallReflect();
+    boxes.forEach(box => {
+      box.draw();
+      box.checkBall();
+    });
     timerId = setTimeout(() => {
       update();
-    }, 20);
+    }, 100);
   }
-  
 
   gameInit();
 
+
+  
   function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  function reflectRand() {
+    // 反射速度にこの少数をかけると不安定
+    let num = Math.random() * (1.05 - 0.93) + 0.93;
+    num *= 100;
+    num = Math.round(num);
+    return num / 100
+    // だんだん遅くなる
   }
 });
