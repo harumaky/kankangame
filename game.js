397261f82524042583e0165d27b15a033e8a03b7
harumaky <harumakiprog@gmail.com>
2020-05-18 14:32:32 +0900

    ES5化（classを代替）

--- game.js
+++ game.js
@@ -5,10 +5,10 @@ $(function() {
   //   e.preventDefault()
   // }, { passive: false });
 
-  const dpr = devicePixelRatio;
-  let canvas = document.getElementById('game-field');
+  var dpr = devicePixelRatio;
+  var canvas = document.getElementById('game-field');
   if (!canvas || !canvas.getContext) { return false };
-  let ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext('2d');
 
   // コンテキストの初期値設定
   canvas.width = 300;
@@ -22,32 +22,30 @@ $(function() {
   canvas.style.width = String(canvas.width / dpr) + "px";
   canvas.style.height = String(canvas.height / dpr) + "px";
 
-  let theBall, // ボールのインスタンス
+  var theBall, // ボールのインスタンス
       initBall, // ボールの初期Xランダム
       updateID, // 描画更新update()をコールバックするためのsetTimeout
       deadZone; // 下部のゲームアウト領域インスタンス
-  let num = 10; // 一つのボックスのHP？の初期値
-  let boxes = []; // ボックスのインスタンス格納配列
-  let updateInterval = 8; // update()更新間隔8(ms)
-  let score = 0;
-  let playing = false;
-  let theArrow; // アローのインスタンス
+  var num = 10; // 一つのボックスのHP？の初期値
+  var boxes = []; // ボックスのインスタンス格納配列
+  var updateInterval = 8; // update()更新間隔8(ms)
+  var score = 0;
+  var playing = false;
+  var theArrow; // アローのインスタンス
 
-  class Ball {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.vx;
-      this.vy;
-      this.r = 5; // 半径固定5
-    }
-    draw() {
+  var Ball = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx;
+    this.vy;
+    this.r = 5; // 半径固定5
+    this.draw = function() {
       ctx.beginPath();
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, true);
       ctx.fill();
     }
-    move() {
+    this.move = function() {
       this.x += this.vx;
       this.y += this.vy;
       // 右、左衝突
@@ -66,8 +64,7 @@ $(function() {
   }
 
   // ボックス（ブロックにした方がよかったなと後悔）
-  class Box {
-    constructor(x, y, num, id) {
+  var Box = function(x, y, num, id) {
       this.x = x;
       this.y = y;
       this.num = num; // box内の数字
@@ -75,66 +72,66 @@ $(function() {
       this.collisioned = false; // 衝突判定が出た場合true
       this.afterCollision = 0; // 衝突判定後の描画更新回数
       this.debugReflect = false;
-    }
-    determineColor() {
-      if(this.collisioned && this.afterCollision < 20) {
-        this.color = 'red'; // 衝突後、20回の間は背景redに
-      } else {
-        this.hue = this.num * 12 + 120; // numに応じて背景色設定
-        this.color = `hsl(${this.hue}, 50%, 60%)`
-        this.collisioned = false;
-      }
-    }
-    draw() {
-      this.determineColor();
-      ctx.fillStyle = this.color;
-      ctx.fillRect(this.x, this.y, 30, 30);
-      // 以下num表示を更新
-      ctx.fillStyle = 'white';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillText(this.num, this.x + 15, this.y + 15);
-
-      this.afterCollision++; // 衝突後の描画カウント
-    }
-    collision() {
-      this.collisioned = true;
-      this.afterCollision = 0;
-      // 衝突フラッグを立て、この先20回の描画後に解除
-      this.num--;
-      score++;
-      $('#score').text(score);
-      if (this.num === 0) this.deleteBox();
-    }
-    deleteBox() {
-      let thisId = this.id;
-      // boxesの中のそれぞれのインスタンスから、idが消去したいボックスと同じものを探す
-      boxes.filter(function(box, index) {
-        if (box.id === thisId) {
-          boxes.splice(index, 1);
-          // ここで削除。それぞれのboxインデックス番号が1減る
-          // よって次も初期化時は、インデックスに依存せず、設定した固有のidで消したいboxを探す
-        }
-      });
+  }
+  // boxは何個も作られるからメソッドはプロトタイプチェーンで
+  Box.prototype.determineColor = function() {
+    if(this.collisioned && this.afterCollision < 20) {
+      this.color = 'red'; // 衝突後、20回の間は背景redに
+    } else {
+      this.hue = this.num * 12 + 120; // numに応じて背景色設定
+      this.color = `hsl(${this.hue}, 50%, 60%)`
+      this.collisioned = false;
     }
   }
+  Box.prototype.draw = function() {
+    this.determineColor();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, 30, 30);
+    // 以下num表示を更新
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(this.num, this.x + 15, this.y + 15);
+    this.afterCollision++; // 衝突後の描画カウント
+  }
+  Box.prototype.collision = function() {
+    this.collisioned = true;
+    this.afterCollision = 0;
+    // 衝突フラッグを立て、この先20回の描画後に解除
+    this.num--;
+    score++;
+    $('#score').text(score);
+    if (this.num === 0) this.deleteBox();
+  }
+  Box.prototype.deleteBox = function() {
+    var thisId = this.id;
+    // boxesの中のそれぞれのインスタンスから、idが消去したいボックスと同じものを探す
+    boxes.filter(function(box, index) {
+      if (box.id === thisId) {
+        boxes.splice(index, 1);
+        // ここで削除。それぞれのboxインデックス番号が1減る
+        // よって次も初期化時は、インデックスに依存せず、設定した固有のidで消したいboxを探す
+      }
+    });
+  }
+
   // boxとボールの当たり判定
   function collisionDetecion() {
     boxes.some(box => {
-      let horizontal = isH(theBall.x, theBall.y, box.x, box.y);
-      let vertical = isV(theBall.x, theBall.y, box.x, box.y);
+      var horizontal = isH(theBall.x, theBall.y, box.x, box.y);
+      var vertical = isV(theBall.x, theBall.y, box.x, box.y);
 
       // 衝突判定が出た場合
       if (horizontal || vertical) {
         // 反射後のスピード
-        let nextVX = horizontal ? theBall.vx * -1 : theBall.vx;
-        let nextVY = vertical ? theBall.vy * -1 : theBall.vy;
+        var nextVX = horizontal ? theBall.vx * -1 : theBall.vx;
+        var nextVY = vertical ? theBall.vy * -1 : theBall.vy;
         // 次の座標x,y計算
-        let nextX = theBall.x + nextVX;
-        let nextY = theBall.y + nextVY;
+        var nextX = theBall.x + nextVX;
+        var nextY = theBall.y + nextVY;
         // 次のフレームでの判定
-        let NextHorizontal = isH(nextX, nextY, box.x, box.y);
-        let NextVerticval = isH(nextX, nextY, box.x, box.y);
+        var NextHorizontal = isH(nextX, nextY, box.x, box.y);
+        var NextVerticval = isH(nextX, nextY, box.x, box.y);
 
         if (horizontal && NextHorizontal) {
           // もし、連続して左右判定がでるとボックス辺付近で反射し続けるから、上下に当たったことにする
@@ -159,49 +156,48 @@ $(function() {
   }
   // a,b=>ballのx,y c,d=>boxの原点(左上)  
   function isH(a, b, c, d) { // 水平（左右）面に当たったか
-    let part1 = b > -a + c + d + 30 && b < a - c + d && a < c + 30;
-    let part2 = b < -a + c + d + 30 && b > a - c + d && a > c;
+    var part1 = b > -a + c + d + 30 && b < a - c + d && a < c + 30;
+    var part2 = b < -a + c + d + 30 && b > a - c + d && a > c;
     return part1 || part2;
   }
   function isV(a, b, c, d) { // 鉛直面
-    let part1 = b < -a + c + d + 30 && b < a - c + d && b > d;
-    let part2 = b > -a + c + d + 30 && b > a - c + d && b < d + 30;
+    var part1 = b < -a + c + d + 30 && b < a - c + d && b > d;
+    var part2 = b > -a + c + d + 30 && b > a - c + d && b < d + 30;
     return part1 || part2;
   }
 
-  class DeadZone {
-    draw() {
+  var DeadZone = function() {
+    this.draw = function() {
       ctx.fillStyle = theBall.y <= 450 ? 'rgba(255, 0, 0, 0.4)' : 'rgba(255, 0, 0, 0.8)';
       ctx.fillRect(0, 450, canvas.width, 50);
     }
   }
 
   // アロー（下の矢印制御）(canvasではなくDOM -> スタイルはcss)
-  class Arrow {
-    constructor() {
-      this.deg = -10; // 時計回りが正だから、-10度が初期値
-      this.updateRad = 0; // deg = 80cos(Rad)-90のRad（無限に大きくなってく）
-      this.running = false;
-    }
-    calcRad() {
+  var Arrow = function() {
+    this.deg = -10; // 時計回りが正だから、-10度が初期値
+    this.updateRad = 0; // deg = 80cos(Rad)-90のRad（無限に大きくなってく）
+    this.running = false;
+    
+    this.calcRad = function() {
       return Math.PI / 180 * Math.abs(this.deg);
       // 呼び出された瞬間の角度をラジアンに変換
     }
-    show() {
+    this.show = function() {
       $('#arrow').css('transform', `translateX(${initBall - 150}px) rotate(${this.deg}deg)`).show(); // initBallの位置に合うようにずらしてから表示
     }
-    draw() {
+    this.draw = function() {
       // 角度は-10 ~ -170度間を、cosカーブの振動に合わせて更新
       this.deg = 80 * Math.cos(this.updateRad) - 90;
       $('#arrow').css('transform', `translateX(${initBall - 150}px) rotate(${this.deg}deg)`);
       this.updateRad = this.updateRad + Math.PI / 270;
       this.running = true;
     }
-    update() {
-      let that = this;
+    this.update = function() {
+      var that = this;
       this.updateID = setInterval(function() { that.draw() }, 30);
     }
-    stop() {
+    this.stop = function() {
       clearInterval( this.updateID );
       this.running = false
     }
@@ -244,7 +240,7 @@ $(function() {
         theArrow.stop(); 
         $('#arrow').fadeOut();
         // console.log(theArrow.deg); // デバッグ用
-        let rad = theArrow.calcRad(); // この瞬間の角度をラジアンに変換
+        var rad = theArrow.calcRad(); // この瞬間の角度をラジアンに変換
         theBall.vx = 2 * Math.cos(rad); // ボールの初速度設定(vx, vy)
         theBall.vy = -2 * Math.sin(rad);
         updateID = setInterval(update, updateInterval); // スタート
@@ -272,43 +268,43 @@ $(function() {
 
   function setBoxes() {
     // 上段
-    for (let i = 0; i < 10; i++) {
+    for (var i = 0; i < 10; i++) {
       boxes.push( new Box(30 * i, 0, num, boxes.length) )
     }
     // 左列
-    for (let i = 0; i < 12; i++) {
+    for (var i = 0; i < 12; i++) {
       boxes.push( new Box(0, 30 * i + 30, num, boxes.length) )
     }
     // 2列目
-    for (let i = 0; i < 8; i++) {
+    for (var i = 0; i < 8; i++) {
       boxes.push( new Box(45, 40 * i + 40, num, boxes.length) )
     }
     // 3列目
-    for (let i = 0; i < 8; i++) {
+    for (var i = 0; i < 8; i++) {
       boxes.push( new Box(90, 40 * i + 50, num, boxes.length) )
     }
     // 4列目
-    for (let i = 0; i < 8; i++) {
+    for (var i = 0; i < 8; i++) {
       boxes.push( new Box(135, 40 * i + 40, num, boxes.length) )
     }
     // 5列目
-    for (let i = 0; i < 8; i++) {
+    for (var i = 0; i < 8; i++) {
       boxes.push( new Box(180, 40 * i + 50, num, boxes.length) )
     }
     // 6列目
-    for (let i = 0; i < 8; i++) {
+    for (var i = 0; i < 8; i++) {
       boxes.push( new Box(225, 40 * i + 40, num, boxes.length) )
     }
     // 右列
-    for (let i = 0; i < 12; i++) {
+    for (var i = 0; i < 12; i++) {
       boxes.push( new Box(270, 30 * i + 30, num, boxes.length) )
     }
     // 下段左
-    for (let i = 0; i < 3; i++) {
+    for (var i = 0; i < 3; i++) {
       boxes.push( new Box(30 * i, 390, num, boxes.length) )
     }
     // 下段右
-    for (let i = 0; i < 3; i++) {
+    for (var i = 0; i < 3; i++) {
       boxes.push( new Box(30 * i + 210, 390, num, boxes.length) )
     }
   }
